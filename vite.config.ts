76e0fc71446c668007ff86dade09cdba15a34f48
Tvrtko <tvrtkobabic2@gmail.com>
2026-03-22 15:26:08 +0100

    Rename repo to habits and update base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  base: "/habbits/",
+  base: "/habits/",
   plugins: [
     react(),
     tailwindcss(),
